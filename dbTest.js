a8bea2cdfda3a8cea084e443c6636517598797c4
liamholland <114741709+liamholland@users.noreply.github.com>
2022-11-11 12:21:13 +0000

    Update dbTest.js

--- dbTest.js
+++ dbTest.js
@@ -5,9 +5,13 @@ test();
 async function test(){
     // await db.registerAccount("liamholland02", "2002", "liamholland02@gmail.com");
     // await db.registerAccount("definitely_not_liam", "2001", "liam.holland5@nuigalway.ie");
-    const id = await db.getID("liamholland02");
-    db.updateProfile(id, "name", "Liam Holland");
-    db.updateProfile(id, "bio", "I am a user");
-    db.updateProfile(id, "dob", "07/04/2002");
-    console.log(await db.getProfileInfo(id), await db.getProfileInfo(id, "name"), await db.getProfileInfo(id, "dob"));
+    // const id = await db.getID("liamholland02");
+    // db.updateProfile(id, "name", "Liam Holland");
+    // db.updateProfile(id, "bio", "I am a user");
+    // db.updateProfile(id, "dob", "07/04/2002");
+    // console.log(await db.getProfileInfo(id), await db.getProfileInfo(id, "name"), await db.getProfileInfo(id, "dob"));
+    // const id2 = await db.getID("definitely_not_liam");
+    // db.updateProfile(id2, "name", "Liam");
+    // console.log(await db.getProfileInfo(await db.getID("definitely_not_liam")));
+    db.getArtistIDs("Liam");
 }
