86fef567093fec5fec113016badd8aa15dfe09fb
FionnMcA <114741890+FionnMcA@users.noreply.github.com>
2022-11-11 12:29:07 +0000

    Add files via upload

---
+++ MusicRecJS.js
@@ -0,0 +1,23 @@
+function openCreateAccountPopup() {
+    document.getElementById("CreateAccountPopup").style.display = "block";
+  }
+function closeCreateAccountPopup() {
+    document.getElementById("CreateAccountPopup").style.display = "none";
+  }
+
+
+  function openLoginPopup() {
+    document.getElementById("LoginPopup").style.display = "block";
+  }
+  function closeLoginPopup() {
+    document.getElementById("LoginPopup").style.display = "none";
+  }
+
+  function openUserProfile() {
+    document.getElementById("UserProfileID").style.display = "block";
+  }
+
+  function closeUserProfile(){
+    document.getElementById("UserProfileID").style.display = "none";
+
+  }
