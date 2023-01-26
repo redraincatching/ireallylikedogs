3860645e263bb5f7f35406444b1ceeab94cca060
liamholland <114741709+liamholland@users.noreply.github.com>
2023-01-26 15:32:56 +0000

    Add files via upload
    Insert this code into the index file in the functions folder to have all of the DB functions (for the moment)

---
+++ index.js
@@ -0,0 +1,124 @@
+const functions = require("firebase-functions");
+let admin = require("firebase-admin");
+let serviceAccount = require("./test-project-3d277-firebase-adminsdk-qfyec-e57396a808.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+//register an account on the DB
+//FORMAT: URL...?text={"uname":"uname","id":"id","email":"email","password":"password"}
+exports.registerAccount = functions.https.onRequest(async (req, res) => {
+  //parse the JSON
+  const json = JSON.parse(req.query.text);
+  //TODO: implement auto id counter
+  const newUname = json.uname;
+  const newID = json.id;
+  const newEmail = json.email;
+  const pswd = json.password;
+
+  //access the Accounts collection and add the required info under the username
+  await db.collection("Accounts").doc(newUname).set({
+    id: newID,
+    email: newEmail,
+    password: pswd
+  }).then(() => {
+    //initialise the Profile for the user - this can be anything
+    db.collection("Profiles").doc(newID).set({
+      username: newUname,
+      pfp: null,
+    });
+    res.json({ result: `Created account for username ${newUname}` });
+  });
+});
+
+//function to login - temporary until firebase auth is set up
+//successful login should be followed with a call to getProfileInfo
+//FORMAT: URL...?text={"username":"uname","password":"password"}
+exports.login = functions.https.onRequest(async (req, res) => {
+  const json = JSON.parse(req.query.text);
+  const uname = json.username;
+  const pswd = json.password;
+
+  //access the account and check the password is correct
+  await db.collection("Accounts").doc(uname).get().then((docsnap) => {
+    if (docsnap.exists) {
+      if (docsnap.data()["password"] == pswd) {
+        res.json({ result: "login successful", bool: true });
+      }
+      else {
+        res.json({ result: "incorrect username for password", bool: false});
+      }
+    }
+    else {
+      res.json({ result: "user does not exist", bool: false });
+    }
+  });
+});
+
+//update any part of a profile with a provided id, field and value
+//FORMAT: URL...?text={"id":"id","field":"field","value":"value"}
+//^ must provide a valid ID
+exports.updateProfile = functions.https.onRequest(async (req, res) => {
+  //parse the JSON
+  const json = JSON.parse(req.query.text);
+  const id = json.id;
+  const field = json.field;
+  const value = json.value;
+
+  //get a reference to the profile
+  const profileRef = db.collection("Profiles").doc(id);
+  await profileRef.get().then((docSnap) => {
+    if (docSnap.exists) {
+      let updateData = {};  //create an empty object
+      updateData[field] = value;  //assign the value to a field in the object
+
+      //use a firebase feature to update the data
+      //only updates based on matching fields, does not overwrite the entire document
+      profileRef.update(updateData).then(() => {
+        res.json({ result: `Updated data regarding ${field} with ${value}` });
+      });
+    }
+    else {
+      res.json({ result: "Failed to find document" });
+    }
+  });
+});
+
+//function dedicated to receiving an ID of a given username
+//FORMAT: URL...?text=uname
+exports.getID = functions.https.onRequest(async (req, res) => {
+  //access the document relating to the 
+  const ret = await db.collection("Accounts").doc(req.query.text).get().then((docSnap) => {
+    if (docSnap.exists) {
+      return docSnap.data()["id"];
+    }
+    else {
+      return `Failed to find user ${req.query.text}`;
+    }
+  });
+  res.json(ret);
+});
+
+//function to provide all the profile info, or just one field, based on the input
+//FORMAT: URL...?text={"id":"id","field":"field"}
+//for all info the field should be null without quotes
+exports.getProfileInfo = functions.https.onRequest(async (req, res) => {
+  //parse the JSON
+  const json = JSON.parse(req.query.text);
+  const id = json.id;
+  const field = json.field;
+
+  //access the appropriate document in the profiles collection
+  const ret = await db.collection("Profiles").doc(id).get().then((docSnap) => {
+    if (docSnap.exists) {
+      //return the entire profile if the field is null, otherwise return the desired field
+      return field == null ? docSnap.data() : docSnap.data()[field];
+    }
+  });
+  res.json(ret);
+});
