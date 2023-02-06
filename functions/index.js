c9146d28b7fe93ce874da476a8b72ee73bbcd611
liamholland <114741709+liamholland@users.noreply.github.com>
2023-02-06 17:50:16 +0000

    DB Functions

--- functions/index.js
+++ functions/index.js
@@ -1,109 +1,134 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+let functions = require("firebase-functions");
+let admin = require("firebase-admin");
+let cors = require("cors")({origin: true});
+let serviceAccount = require("./test-project-3d277-firebase-adminsdk-qfyec-e57396a808.json");
 
-var modal = document.getElementById("myModal"); // Get the modal
-var btn = document.getElementById("myBtn"); // Get the button that opens the modal
-var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  modal1.style.display = "none";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-function closesignin(){
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-var modal1 = document.getElementById("myModal1");
-var btn1 = document.getElementById("myBtn1"); // Get the button that opens the modal
-var span1 = document.getElementsByClassName("close1")[0]; // Get the <span> element that closes the modal
-
-// When the user clicks the button, open the modal 
-btn1.onclick = function() {
-  modal1.style.display = "block";
-  modal.style.display = "none";
-}
-
-
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-  modal1.style.display = "none";
-}
-
-span1.onclick = function() {
-  modal1.style.display = "none";
-}
-
-function closesignin1(){
-  modal1.style.display = "none";
-}
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase again!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
 });
+const db = admin.firestore();
 
-exports.isLoggedIn = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Not logged in");
-});
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.echo = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("The data passed in is " + request.query.data);
-});
+//replace everything in the {} for the formats
 
-// Accept comment and return the same comment to the user
-exports.postcomment = functions.https.onRequest((request, response) => {
-    // 1. Receive comment data in here from user POST request
-    // 2. Connect to our Firestore database
-    const currentTime = admin.firestore.Timestamp.now();
-    request.body.timestamp = currentTime;
-
-    return admin.firestore().collection('comments').
-    add(request.body).then(()=>{
-        response.send("Saved in the database");
+//register an account on the DB
+//FORMAT: URL...?uname={uname}&id={id}&email={email}&password={password}
+exports.registerAccount = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    //get variables
+    const newUname = req.query.uname;
+    //TODO: implement auto id counter
+    const newID = req.query.id;
+    const newEmail = req.query.email;
+    const pswd = req.query.password;
+  
+    //access the Accounts collection and add the required info under the username
+    await db.collection("Accounts").doc(newUname).set({
+      id: newID,
+      email: newEmail,
+      password: pswd
+    }).then(() => {
+      //initialise the Profile for the user - this can be anything
+      db.collection("Profiles").doc(newID).set({
+        username: newUname,
+        pfp: "Not Set",
+      });
+      res.json({ result: `Created account for username ${newUname}` });
     });
-
+  });
 });
 
-exports.getcomments = functions.https.onRequest((request, response) => {
-
-    // 1. Connect to our Firestore database
-    let myData = [];
-    return admin.firestore().collection('comments').orderBy('timestamp').get().then((snapshot) => {
-
-        if (snapshot.empty) {
-            console.log('No matching documents.');
-            response.send('No data in database');
-            return;
+//function to login - temporary until firebase auth is set up
+//successful login should be followed with a call to getProfileInfo
+//FORMAT: URL...?username={username}&password={password}
+exports.login = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    //get variables
+    const uname = req.query.username;
+    const pswd = req.query.password;
+  
+    //access the account and check the password is correct
+    await db.collection("Accounts").doc(uname).get().then((docsnap) => {
+      if (docsnap.exists) {
+        if (docsnap.data()["password"] == pswd) {
+          res.json({ result: "login successful", bool: true });
         }
+        else {
+          res.json({ result: "incorrect username for password", bool: false});
+        }
+      }
+      else {
+        res.json({ result: "user does not exist", bool: false });
+      }
+    });
+  }); 
+});
 
-        snapshot.forEach(doc => {
-            myData.push(doc.data());
-        });
+//update any part of a profile with a provided id, field and value
+//FORMAT: URL...?id={id}&field={field}&value={value}
+//^ must provide a valid ID
+exports.updateProfile = functions.https.onRequest(async (req, res) => {
+  //get variables
+  const id = req.query.id;
+  const field = req.query.field;
+  const value = req.query.value;
 
-        // 2. Send data back to client
-        response.send(myData);
-    })
+  //get a reference to the profile
+  const profileRef = db.collection("Profiles").doc(id);
+  await profileRef.get().then((docSnap) => {
+    if (docSnap.exists) {
+      let updateData = {};  //create an empty object
+      updateData[field] = value;  //assign the value to a field in the object
+
+      //use a firebase feature to update the data
+      //only updates based on matching fields, does not overwrite the entire document
+      profileRef.update(updateData).then(() => {
+        res.json({ result: `Updated data regarding ${field} with ${value}` });
+      });
+    }
+    else {
+      res.json({ result: "Failed to find document" });
+    }
+  });
+});
+
+//function dedicated to receiving an ID of a given username
+//FORMAT: URL...?uname={uname}
+exports.getID = functions.https.onRequest(async (req, res) => {
+  //access the document relating to the 
+  const ret = await db.collection("Accounts").doc(req.query.uname).get().then((docSnap) => {
+    if (docSnap.exists) {
+      return docSnap.data()["id"];
+    }
+    else {
+      return `Failed to find user ${req.query.uname}`;
+    }
+  });
+  res.json(ret);
+});
+
+//function to provide all the profile info, or just one field, based on the input
+//FORMAT: URL...?id={id}&field={field}
+//for all info the field parameter should be left out
+exports.getProfileInfo = functions.https.onRequest(async (req, res) => {
+  //get the variables
+  const id = req.query.id;
+  const field = req.query.field;
+
+  //access the appropriate document in the profiles collection
+  const ret = await db.collection("Profiles").doc(id).get().then((docSnap) => {
+    if (docSnap.exists) {
+      //return the entire profile if the field is null, otherwise return the desired field
+      if(field == null){
+        return docSnap.data();
+      }
+      else{
+        const data = docSnap.data()[field];
+        return data == null ? "Unknown Field" : data;
+      }
+    }
+  });
+  res.json(ret);
 });
