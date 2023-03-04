c1b807e7b1c2004cc311e57391d89d84b2305aad
liamholland <114741709+liamholland@users.noreply.github.com>
2023-03-04 14:40:35 +0000

    Updated Login and Sign Up Functionality

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,6 +1,7 @@
 <script>
 import app from "../../api/firebase";
-import {getFunctions, httpsCallable, connectFunctionsEmulator} from "firebase/functions";
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from "firebase/functions";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, connectAuthEmulator } from "firebase/auth";
 
 export default {
   data() {
@@ -8,48 +9,116 @@ export default {
       email: "",
       username: "",
       password: "",
-      loginsuccess: true
+      loggedIn: false,
+      loading: false,
+      loadingBar: null,
     }
-  },  
+  },
   methods: {
+    //closes the sign in or sign up pop ups
+    //get an array of all the pop ups with the style of a user identifcation section
+    //select the correct one based on the index (they will appear on the page in the order of their definitions in the html)
     closesignin(i) {
       let elms = document.querySelectorAll('.modal');
       elms[i].style.display = "none";
     },
-  
-    opensignin(i){
+
+    //opens the sign in or sign up pop ups
+    //get an array of all the pop ups with the style of a user identifcation section
+    //select the correct one based on the index 
+    // (the popups will appear in the array in the order of their definitions in the html)
+    opensignin(i) {
       let elms = document.querySelectorAll('.modal');
       elms[i].style.display = "flex";
     },
 
-    login(){
-      const functions = getFunctions(app);
-      connectFunctionsEmulator(functions, "localhost", 5001);
-      const login = httpsCallable(functions, "login");
-      const getID = httpsCallable(functions, "getID");
-      const getProfileInfo = httpsCallable(functions, "getProfileInfo"); 
-      login({"username": this.username, "password": this.password}).then((succ) => {
-        this.loginsuccess = succ.data.bool;
-        if(succ.data.bool == true){
-          getID({"uname": this.username}).then((res) => {
-            getProfileInfo({"id": res.data.id}).then((info) => {
-              console.log(info.data);
-              this.closesignin(0);
-            });
-          });
-        }
-      });
+    //show the loading bar
+    showLoad(){
+      if(!this.loading){
+        this.loadingBar = this.$loading.show();
+        this.loading = true;
+      }
     },
 
-    register(){
+    //hide the loading bar
+    hideLoad(){
+      this.loadingBar.hide();
+      this.loading = false;
+    },
+
+    //regex test to see if its an email
+    isEmail(e){
+      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(e);
+    },
+
+    //login function
+    //works for email or username
+    login() {
+      this.showLoad(); //loading
+
+      //get the id if the email is a username
+      if(!this.isEmail(this.email)){
+        //get the id from the username
+      }
+
+      //get necessary components
+      const auth = getAuth(app);
       const functions = getFunctions(app);
+      
+      //TODO: remove emulator line when deploying
       connectFunctionsEmulator(functions, "localhost", 5001);
-      const register = httpsCallable(functions, "registerAccount");
-      let id = Math.floor(Math.random() * 100);
-      register({"uname": this.username, "id": id, "email": this.email, "pswd": this.password}).then((result) => {
-        console.log(result.data);
-        this.closesignin(1);
-        this.login();
+      // connectAuthEmulator(auth, "http://localhost:5003");
+      
+      //sign in with firebase auth using email and password
+      signInWithEmailAndPassword(auth, this.email, this.password).then((userCred) => {
+        const user = userCred.user;
+
+        //define getProfileInfo
+        const getProfileInfo = httpsCallable(functions, "getProfileInfo");
+        //get the profile information of the user once their are signed in
+        //stored under the users id
+        getProfileInfo({ "id": user.uid }).then((info) => {
+          console.log(info.data);
+          this.closesignin(0); //close the sign in popup
+          this.loggedIn = true;
+          this.hideLoad();  //done loading
+          this.$router.push({path: '/AccountPage'});
+        });
+      });
+
+      this.hideLoad();  //done loading
+    },
+
+    //register an account
+    //calls login after successful execution
+    register() {
+      this.showLoad();  //loading
+
+      //get auth component
+      const auth = getAuth(app);
+      const functions = getFunctions(app);
+      
+      //TODO: remove emulator line when deploying
+      connectFunctionsEmulator(functions, "localhost", 5001);
+      // connectAuthEmulator(auth, "http://localhost:5003");
+      
+      //create a user in firebaser
+      createUserWithEmailAndPassword(auth, this.email, this.password).then((userCred) => {
+        const user = userCred.user;
+        
+        //define the register function
+        const register = httpsCallable(functions, "registerAccount");
+        
+        //register function sets up the database for this users data
+        register({ "id": user.uid, "username": this.username }).then((result) => {
+          console.log(result.data);
+          this.closesignin(1);  //close the register popup
+          this.login(); //log the user in
+        });
+      }).catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+        this.hideLoad();  //done loading
       });
     }
   }
@@ -57,226 +126,130 @@ export default {
 </script>
 
 <template>
-
-
-    <header id="header" class="header d-flex align-items-center">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-   
-
-      <div id="myModal" class="modal"> <!-- Sign in popup  -->
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close" @click="closesignin(0)">&times;</span>
-    <div v-if="!loginsuccess">
-      <label style="color:red;">Login Failed</label>
-    </div>
-    <p style="font-size: 40px;">Sign in</p>
-    <label style="color: #949494;">Username</label>
-    <input style="height: 40px; border-radius: 7.5px;" required v-model="username"><br>
-    <label style="color: #949494;"> Password</label>
-    <input style="height: 40px; border-radius: 7.5px;" required v-model="password"><br>
-    <a @click="login" class="btn-get-started">Sign in</a>
-  </div>
-</div>
-
-<div id="myModal1" class="modal"> <!-- create account popup  -->
+  
+  
+  <div id="myModal" class="modal"> <!-- Sign in popup  -->
     <!-- Modal content -->
-    <div class="modal-content" style="height:545px">
-      <span class="close" @click="closesignin(1)">&times;</span><br><br>
-      <p style="font-size: 40px;">Create account</p>
-      <label style="color: #949494;" >Email</label><br>
-      <input style="height: 40px; border-radius: 7.5px;"  required v-model="email"><br><br>
-      <label style="color: #949494;" >Username</label><br>
-      <input style="height: 40px; border-radius: 7.5px;"  required v-model="username"><br><br>
-      <label style="color: #949494;">Password</label><br>
-      <input style="height: 40px; border-radius: 7.5px;"  required v-model="password"><br><br>
+    <div style="align-items: center" class="modal-content">
+      <span class="close" @click="closesignin(0)">&times;</span>
+      <p style="font-size: 40px; color: white">Sign in</p>
+      <label style="color: #949494;">Email or Username</label>
+      <input style="height: 40px; border-radius: 7.5px;" required v-model="email"><br>
+      <label style="color: #949494;"> Password</label>
+      <input style="height: 40px; border-radius: 7.5px;" required v-model="password"><br>
+      <a @click="login" class="btn-get-started">Sign in</a>
+    </div>
+  </div>
+
+  <div id="myModal1" class="modal"> <!-- create account popup  -->
+    <!-- Modal content -->
+    <div style="align-items: center" class="modal-content">
+      <span class="close" @click="closesignin(1)">&times;</span>
+      <p style="font-size: 40px; text-align: center; color: white">Create account</p>
+      <label style="color: #949494;">Email</label>
+      <input style="height: 40px; border-radius: 7.5px;" required v-model="email"><br>
+      <label style="color: #949494;">Username</label>
+      <input style="height: 40px; border-radius: 7.5px;" required v-model="username"><br>
+      <label style="color: #949494;">Password</label>
+      <input style="height: 40px; border-radius: 7.5px;" required v-model="password"><br>
       <a @click="register" class="btn-get-started">Create</a>
     </div>
   </div>
 
-<router-link to="/">
-<img src="../assets/img/cratedigger_banner.png" alt="Crate Digger" width="150" height="60"><!--Logo-->
-</router-link>
 
-      
-        <nav id="navbar" class="navbar">
-       
-    
-        <router-link to="/">Home</router-link>&nbsp;&nbsp;&nbsp;
-        <li><a class="myBtn1" id="myBtn1" @click="opensignin(1)">Sign Up</a></li>&nbsp;&nbsp;&nbsp;
-        <li><a class="myBtn" id="myBtn" @click="opensignin(0)">Login</a></li>&nbsp;&nbsp;&nbsp;
-        <router-link to="/AboutUs">About Us</router-link>&nbsp;&nbsp;&nbsp;
-        <router-link to="/SearchPage">Search</router-link>&nbsp;&nbsp;&nbsp;
-        <router-link to="/AccountPage">Account</router-link>
-        </nav>
-        </div>
-        </header>
-        
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <nav class="navbar navbar-expand-sm " aria-label="Third navbar example" style="background-color:black">
+    <div class="container-fluid">
+      <router-link to="/">
+
+        <img src="../assets/img/cratedigger_banner.png" alt="Crate Digger" width="150" height="60"><!--Logo-->
+      </router-link>
+
+      <button style="background-color: grey" class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample03" style="text-align: center; margin-left:40%">
+        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+          <li class="nav-item" style="margin-right: 30px;">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li v-if="!loggedIn" class="nav-item" style="list-style-type: none; margin-right: 30px;"><a class="myBtn1" id="myBtn1"
+              @click="opensignin(1)">Sign Up</a></li>
+          <li v-if="!loggedIn" class="nav-item" style="list-style-type: none; margin-right: 30px;"><a class="myBtn" id="myBtn"
+              @click="opensignin(0)">Login</a></li>
+          <li class="nav-item" style="margin-right: 30px;">
+            <router-link to="/AboutUs">About Us</router-link>
+          </li>
+          <li class="nav-item" style="margin-right: 30px;">
+            <router-link to="/SearchPage">Search</router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link to="/AccountPage">Account</router-link>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
 </template>
 <style scoped>
+.nav-item {
+  color: rebeccapurple;
+}
 
-.header{
-z-index: 997;
+.myBtn1 {
+  list-style-type: none;
+}
+
+.header {
+  z-index: 997;
   position: absolute;
-  padding: 30px 0;
+
   top: 0;
   left: 0;
   right: 0;
   background-color: black;
   list-style-type: none;
 }
+
 .navbar {
-    padding: 0;
-  }
-
-.navbar ul {
-        list-style-type: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    list-style: none;
-    align-items: center;
-  }
-.navbar li {
-    position: relative;
-  }
-
-  .navbar>ul>li {
-    white-space: nowrap;
-    padding: 10px 0 10px 28px;
-  }
-
-  .navbar a,
-  .navbar a:focus {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 3px;
-    font-family: "Roboto", sans-serif;
-    font-size: 20px;
-    font-weight: 500;
-    text-decoration: none;
-    color: #1DB954;
-
-    text-transform: uppercase;
-    white-space: nowrap;
-    transition: 0.3s;
-    position: relative;
-  }
-
-  .navbar a i,
-  .navbar a:focus i {
-    font-size: 22px;
-    line-height: 0;
-    margin-left: 5px;
-  }
-
-  .navbar>ul>li>a:before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    bottom: -6px;
-    left: 0;
-    background-color: black;
-    visibility: hidden;
-    width: 0px;
-    transition: all 0.3s ease-in-out 0s;
-  }
-
-  .navbar a:hover:before,
-  .navbar li:hover>a:before,
-  .navbar .active:before {
-    visibility: visible;
-    width: 100%;
-  }
-
-  .navbar a:hover,
-  .navbar .active,
-  .navbar .active:focus,
-  .navbar li:hover>a {
-    color: white;
-  }
-
-  .navbar .dropdown ul {
-    display: block;
-    position: absolute;
-    left: 28px;
-    top: calc(100% + 30px);
-    margin: 0;
-    padding: 10px 0;
-    z-index: 99;
-    opacity: 0;
-    visibility: hidden;
-    background: #fff;
-    box-shadow: 0px 0px 30px rgba(127, 137, 161, 0.25);
-    transition: 0.3s;
-  }
-
-  .navbar .dropdown ul li {
-    min-width: 200px;
-  }
-
-  .navbar .dropdown ul a {
-    padding: 10px 20px;
-    font-size: 15px;
-    text-transform: none;
-    color: var(--color-default);
-    font-weight: 400;
-  }
-
-  .navbar .dropdown ul a i {
-    font-size: 12px;
-  }
-
-  .navbar .dropdown ul a:hover,
-  .navbar .dropdown ul .active:hover,
-  .navbar .dropdown ul li:hover>a {
-    color: var(--color-primary);
-  }
-
-  .navbar .dropdown:hover>ul {
-    opacity: 1;
-    top: 100%;
-    visibility: visible;
-  }
-
-  .navbar .dropdown .dropdown ul {
-    top: 0;
-    left: calc(100% - 30px);
-    visibility: hidden;
-  }
-
-  .navbar .dropdown .dropdown:hover>ul {
-    opacity: 1;
-    top: 0;
-    left: 100%;
-    visibility: visible;
-  }
-
-@media (min-width: 1280px) and (max-width: 1366px) {
-  .navbar .dropdown .dropdown ul {
-    left: -90%;
-  }
-
-  .navbar .dropdown .dropdown:hover>ul {
-    left: -100%;
-  }
+  padding: 20px 0;
 }
 
-@media (min-width: 1280px) {
+.navbar a,
+.navbar a:focus {
+  display: flex;
+  padding: 0 3px;
+  font-family: "Roboto", sans-serif;
+  font-size: 20px;
+  font-weight: 500;
+  text-decoration: none;
+  color: #1DB954;
 
-  .mobile-nav-show,
-  .mobile-nav-hide {
-    display: none;
-  }
+  text-transform: uppercase;
+
+  transition: 0.3s;
+
 }
 
-.hero{
-    background-color: black;
-    padding-top: 12.5%;
-  }
+
+.navbar a:hover {
+  color: white;
+}
+
+
+
+.hero {
+  background-color: black;
+  padding-top: 12.5%;
+}
 
 .btn-get-started {
   font-weight: 500;
@@ -298,15 +271,13 @@ z-index: 997;
 }
 
 
-.heading:hover{
+.heading:hover {
   color: #1DB954;
 }
 
-.services{
-  text-align: center;
-  
+.services {
   color: white;
-  background-color:black;
+  background-color: black;
   background-size: cover;
   font-size: 14px;
   padding: 80px 0 60px 0;
@@ -314,7 +285,7 @@ z-index: 997;
 }
 
 .section-header {
-  text-align: center;
+
   padding-bottom: 7.5%;
 
   color: white;
@@ -324,69 +295,76 @@ z-index: 997;
   background-color: #262626;
 }
 
-.service-item:hover{
+.service-item:hover {
   color: #1DB954;
 }
-  
-  /* The Modal (background) */
-  .modal {
-    
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 150px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    /* background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.6); /* Black w/ opacity */
-  }
-  
-  /* Modal Content */
-  .modal-content {
-      align-items: left;
-      width: 350px;
-      height: 450px;
-    background-color: #1a1a1a;
-    margin: auto;
-    padding: 15px;
-  
-   
-  }
-  
-  /* The Close Button */
-  .close {
-      position: relative;
-      left: 40%;
-    color: #262626;
-    float: right;
-    font-size: 32px;
-    font-weight: bold;
-  }
-  
-  .close:hover,
-  .close:focus {
-    color: #BFB1A4;
-    text-decoration: none;
-    cursor: pointer;
-  }
+
+/* The Modal (background) */
+.modal {
+
+  display: none;
+  /* Hidden by default */
+  position: fixed;
+  /* Stay in place */
+  z-index: 1;
+  /* Sit on top */
+  padding-top: 10px;
+  /* Location of the box */
+  left: 0;
+  top: 0;
+  /*width: 100%; /* Full width */
+  /*height: 100%; /* Full height */
+  overflow: auto;
+  /* Enable scroll if needed */
+  /* background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.6);
+  /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  align-items: left;
+  width: 350px;
+  height: 500px;
+  background-color: #1a1a1a;
+  margin: auto;
+  padding: 5px;
+
+
+}
+
+/* The Close Button */
+.close {
+  position: relative;
+  left: 40%;
+  color: white;
+  float: right;
+  font-size: 32px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #1DB954;
+  text-decoration: none;
+  cursor: pointer;
+}
 
 .close1:hover,
 .close1:focus {
-color: #BFB1A4;
-text-decoration: none;
-cursor: pointer;
+  color: #1DB954;
+  text-decoration: none;
+  cursor: pointer;
 }
-/* .modal-content {
-    align-items: center;
-    width: 350px;
-    height: 450px;
-  background-color: #1a1a1a;
-  margin: auto;
-  padding: 10px;
 
- 
+/* .modal-content {
+align-items: center;
+width: 350px;
+height: 450px;
+background-color: #1a1a1a;
+margin: auto;
+padding: 10px;
+
+
 } */
 </style>
